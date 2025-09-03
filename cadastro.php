525a077fa5f2c5814e86d918ced6fe33d4191d40
ericbrambilaifc <ericbrambilaifc@gmail.com>
2025-09-03 19:44:07 -0300

    tirar verbosidade

--- cadastro.php
+++ cadastro.php
@@ -2,34 +2,15 @@
 require_once 'src/FilmeDAO.php';
 require_once 'src/SerieDAO.php';
 
-// Verifica se é uma requisição POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    try {
-        $dados = $_POST;
-        
-        // Verifica qual tipo foi selecionado no formulário
-        $tipo = $_POST['tipo'] ?? 'filme'; // Default para filme se não especificado
-        
-        if ($tipo === 'filme') {
-            FilmeDAO::inserir($dados);
-            $mensagem = "Filme cadastrado com sucesso ✅";
-        } elseif ($tipo === 'serie') {
-            SerieDAO::inserir($dados);
-            $mensagem = "Série cadastrada com sucesso ✅";
-        } else {
-            throw new Exception("Tipo inválido selecionado");
-        }
-        
-        echo $mensagem;
-        echo "<br>Voltar para <a href='/adicionarFilme.php'>Adicionar Filme/Série</a>";
-        echo "<br>Ir para <a href='/newDesing.php'>Ver Filmes/Séries</a>";
-        
-    } catch (Exception $e) {
-        echo "Erro no cadastro: " . $e->getMessage();
-        echo "<br>Voltar para <a href='/adicionarFilme.php'>Adicionar Filme/Série</a>";
+if ($_POST) {
+    if ($_POST['tipo'] == 'filme') {
+        FilmeDAO::inserir($_POST);
+        echo "Filme cadastrado!";
+    } else {
+        SerieDAO::inserir($_POST);
+        echo "Série cadastrada!";
     }
-} else {
-    echo "Método de requisição inválido.";
-    echo "<br>Voltar para <a href='/adicionarFilme.php'>Adicionar Filme/Série</a>";
+    
+    echo "<br><a href='adicionarFilme.php'>Voltar</a>";
 }
 ?>
